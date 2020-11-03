d555e2bacfedb59669b04521a797572a000d564d
Adiel Seffrin <adielseffrin@gmail.com>
2020-11-03 20:02:16 -0300

    conectado

--- processador.php
+++ processador.php
@@ -1,5 +1,12 @@
 <?php
 require_once "./infoBD.php";
 
+try{
+  $conn = new PDO("mysql:host=$host;dbname=$db_name",$user,$senha);
+  echo "Deu boa";
+}catch(PDOException $e){
+  die("Erro: ". $e->getMessage());
+}
+
 
 
