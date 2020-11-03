cc582930c315c60c8e7f9960d0e842a42d7bbae6
Adiel Seffrin <adielseffrin@gmail.com>
2020-11-03 19:52:58 -0300

    criando conẽxão com o BD

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+
+</head>
+<body>
+  <div id="batepapo">
+    <div id="mensagens">
+    </div>
+    <div id="acoes">
+      <div id="mensagem">
+      <label for="input_mensagem">Mensagem:</label>
+        <input type="text" id="input_mensagem" name="input_mensagem" />
+      </div>
+      <div id="div_botao_enviar">
+        <input type="button" value="Enviar" id="botao_enviar"/>
+      </div>
+      <div id="botao_atualizar"></div>
+      <div id="informacoes"></div>
+    </div>
+  </div>
+</body>
+</html>
